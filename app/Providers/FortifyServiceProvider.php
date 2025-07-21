9016596b6c1b15fb21a43f417606dad72699f097
Kevin Wangsa <kevinwangsa.kw@gmail.com>
2025-07-21 22:53:10 +0700

    added reset-password page

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -57,5 +57,8 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.forgot-password');
         });
 
+        Fortify::resetPasswordView(function ($request) {
+            return view('auth.reset-password', ['request' => $request]);
+        });
     }
 }
